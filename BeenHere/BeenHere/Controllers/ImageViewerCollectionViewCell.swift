576ac4aa6facc6bfb3066535d32824b154fba30c
Petko Haydushki <Haydushki@Petkos-MacBook-Pro.local>
2018-02-24 11:33:52 +0200

    project added

---
+++ BeenHere/BeenHere/Controllers/ImageViewerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewerCollectionViewCell.swift
+//  BeenHere
+//
+//  Created by Petko Haydushki on 23.02.18.
+//  Copyright © 2018 Petko Haydushki. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewerCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BeenHere/BeenHere/Resources/DetailTableFirstSectionHeader.swift
@@ -0,0 +1,9 @@
+//
+//  DetailTableFirstSectionHeader.swift
+//  BeenHere
+//
+//  Created by Petko Haydushki on 19.02.18.
+//  Copyright © 2018 Petko Haydushki. All rights reserved.
+//
+
+import Foundation
